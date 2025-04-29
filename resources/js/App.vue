b62a96ad6ae205773256fe87c37d7badc75e15dd
JoshuaAbes <joshua.abessolo@heig-vd.ch>
2025-04-29 13:20:46 +0200

    Contenu de base pour App.vue

---
+++ resources/js/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed} from "vue"
+import TheHeader from "./components/TheHeader.vue";
+import Home from "./pages/Home.vue";
+import EditStory from "./pages/EditStory.vue";
+import Game from "./pages/Game.vue";
+
+const routes = {
+    "/": Home,
+    "edit": EditStory,
+    "create": EditStory,
+    "game": Game,
+}
+
+const currentPath = ref(window.location.pathname)
+
+window.addEventListener("hashchange", () => {
+    currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+    return routes[currentPath.value.slice(1).split("-")[0]] || Home
+})
+
+</script>
+<template>
+    <TheHeader></TheHeader>
+    <component :is="currentView"></component>
+</template>
+<style scoped></style>
